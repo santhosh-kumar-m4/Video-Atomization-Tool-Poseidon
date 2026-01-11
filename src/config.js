f3ccb68eeee36d7f3c0d0fd540a741cd639fc1ea
Santhosh Kumar M <santhoshkumar.m@73strings.com>
2026-01-11 13:15:58 +0530

    initialize Angular project and backend structure

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// db config - need to setup postgres
+// using pg library for connection
+
+const { Pool } = require('pg');
+
+// TODO: setup proper connection pool
+// will add this once we have db credentials
+
+module.exports = {
+  // placeholder for now
+};
